5bc71d1199e660d89e767cff4ba6fb84fedfb3f7
fourtimes <57703276+FourTimes@users.noreply.github.com>
2021-11-23 12:23:14 +0530

    Update main.py

--- python/main.py
+++ python/main.py
@@ -6,7 +6,7 @@ REGION = os.environ['REGION']
 # function => to get aws instance id
 def instanceIds():
     ids = []
-    ec2 = boto3.resource('ec2')
+    ec2 = boto3.session.Session(region_name=REGION).resource('ec2')
     for instance in ec2.instances.all():
         ids.append(instance.id)
     return ids
@@ -14,7 +14,7 @@ def instanceIds():
 # function => to delete the aws instance
 def deleteInstance(ids):
     if ids != []:
-        ec2 = boto3.resource('ec2')
+        ec2 = boto3.session.Session(region_name=REGION).resource('ec2')
         ec2.instances.filter(InstanceIds=ids).terminate()
     else:
         print("instance not found.")
